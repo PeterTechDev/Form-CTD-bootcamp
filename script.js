6a153c7b0f2917e86eeb27e2ca577951f8a76658
Peter Leite <peterleite.dev@gmail.com>
2021-10-01 19:56:54 -0300

    inicia estilização

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 let form = document.getElementById("myForm");
+let userBtn = document.getElementById('userBtn');
 
-form.addEventListener('submit', (event)=>{
+
+userBtn.addEventListener('click', (event) => {
     event.preventDefault();
-
+    
     let search = document.getElementById('search').value
     let userName = search.split(' ').join('').toLowerCase()
     let result = document.getElementById('result');
-    
+
     result.innerHTML = '';
 
     fetch("https://api.github.com/users/" + userName)
-    .then((result) => result.json())
-    .then((data)=>{
-        // console.log(data);
-        result.innerHTML = `<img src="${data.avatar_url}" />`
-    })
+        .then((result) => result.json())
+        .then((data) => {
+            // console.log(data);
+            result.innerHTML = `<img src="${data.avatar_url}" />`
+        })
 })
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault()
+    console.log('AHHHH');
+});
