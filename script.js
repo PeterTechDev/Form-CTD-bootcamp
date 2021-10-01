6064b132d2140b1e6c946d6b288c6a6a10950ab6
Peter Leite <peterleite.dev@gmail.com>
2021-10-01 18:11:56 -0300

    refatorando o script

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ form.addEventListener('submit', (event)=>{
     event.preventDefault();
 
     let search = document.getElementById('search').value
+    let userName = search.split(' ').join('').toLowerCase()
+    let result = document.getElementById('result');
     
-    let originalName = search.split(' ').join('')
+    result.innerHTML = '';
 
-    fetch("https://api.github.com/users/"+originalName)
+    fetch("https://api.github.com/users/" + userName)
     .then((result) => result.json())
     .then((data)=>{
-        console.log(data);
-
-        document.getElementById('result')
-        .innerHTML = `<img src="${data.avatar_url}" />`
+        // console.log(data);
+        result.innerHTML = `<img src="${data.avatar_url}" />`
     })
 })
